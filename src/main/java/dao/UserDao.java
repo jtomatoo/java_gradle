fdeedefd8b8b843510e685eb38600fd7816e5972
jtomatoo <jtoamtoo@gmail.com>
2018-12-13 15:48:04 +0900

    init

---
+++ src/main/java/dao/UserDao.java
@@ -0,0 +1,48 @@
+package dao;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+import domain.User;
+
+public class UserDao {
+
+	public void add(User user) throws ClassNotFoundException, SQLException{
+		Class.forName("com.mysql.cj.jdbc.Driver");
+		Connection c = DriverManager.getConnection("jdbc:mysql://localhost/springbook?useSSL=false&serverTimezone=UTC", "spring", "book");
+		
+		PreparedStatement ps = c.prepareStatement("insert into user(id, name, password) value(?, ?, ?)");
+		ps.setString(1, user.getId());
+		ps.setString(2, user.getName());
+		ps.setString(3, user.getPassword());
+		
+		ps.executeUpdate();
+		
+		ps.close();
+		c.close();
+	}
+	
+	public User get(String id) throws ClassNotFoundException, SQLException {
+		Class.forName("com.mysql.cj.jdbc.Driver");
+		Connection c = DriverManager.getConnection("jdbc:mysql://localhost/springbook?useSSL=false&serverTimezone=UTC", "spring", "book");
+		
+		PreparedStatement ps = c.prepareStatement("select * from user where id = ?");
+		ps.setString(1, id);
+		
+		ResultSet rs = ps.executeQuery();
+		rs.next();
+		User user = new User();
+		user.setId(rs.getString("id"));
+		user.setName(rs.getString("name"));
+		user.setPassword(rs.getString("password"));
+		
+		rs.close();
+		ps.close();
+		c.close();
+		
+		return user;
+	}
+}
